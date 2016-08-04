a9e7517634ffd5ed6fd25565c4c50ad62f9485a1
Donjeta Sefa <donjetas1440@afsenyc.org>
2016-08-04 09:41:33 -0400

    create project

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function circle(x, y, diameter){
+  ellipse(x, y, diameter, diameter);
+}
+
+function setup() {
+  createCanvas(1640, 1480);
+  fill(234,53,86);
+  translate(460,100);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+   fill(225,203,166);
+  translate(110,10);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  fill(97,210,214);
+  translate(260,100);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+    fill(237,222,69);
+  translate(430,100);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  fill(155,240,233);
+  translate(260,100);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  fill(255,153,69);
+  translate(430,100);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  fill(225,203,166);
+  translate(65,110);
+  noStroke();
+  for (var i = 0; i < 10; i ++ ){
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+}
+
+function draw() {
+  
+}
